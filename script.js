feb4cb519eb343838f9972eff8e4d8a0b28be1b1
TaviDorvos <octavian.dorvos@yahoo.com>
2021-02-24 12:19:03 +0200

    Adding the mines, flag option

--- script.js
+++ script.js
@@ -25,6 +25,7 @@
     //starting the game
     //removing the 'start' button
     //creating the gameboard
+    //adding the mines
     $('#start').click(function() {
         if (gridInput != "" && minesInput != "") {
             $('#start').remove();
@@ -34,6 +35,39 @@
             for (let i = 0; i < gridInput; i++) {
                 $('#gameboard tr').append('<td></td>');
             }
+            //adding the right-click option
+            $("td").contextmenu(function() {
+                $(this).toggleClass("right-clicked");
+            })
+
+            //click on cell
+            $("td").click(function() {
+                clickOnCell(this);
+            })
+
+            addMines();
         } else alert("Please select the grid and the mines number!");
     })
+
+    //creating the mines randomly
+    function addMines() {
+        for (let i = 0; i < minesInput; i++) {
+            var rowBomb = Math.floor(Math.random() * gridInput);
+            var colBomb = Math.floor(Math.random() * gridInput);
+            console.log(rowBomb, colBomb);
+
+            var $rows = $("#gameboard").find("tr");
+            var $columns = $($rows[rowBomb]).find("td");
+
+            $($columns[colBomb]).addClass("mine");
+        }
+    }
+
+    function clickOnCell( /*parameter*/ ) {
+        console.log(this);
+        if ($(this).hasClass("mine")) {
+            alert("You clicked the mine.\nGame Over!")
+            $(".mine").css("background-color", "black");
+        }
+    }
 })(jQuery)
